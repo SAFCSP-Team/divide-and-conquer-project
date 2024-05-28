d30ef8b7148cb5acaaab242fc853f886405864f2
nourabyte <148945652+nourabyte@users.noreply.github.com>
2024-05-28 13:24:40 +0300

    Create BinarySearch.java

---
+++ src/BinarySearch.java
@@ -0,0 +1,17 @@
+public class BinarySearch {
+
+      /* write your code here */
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+        int target = 16;
+        
+        int index = binarysearch(arr, target);
+        
+        if (index != -1) {
+            System.out.println("Element found at index " + index);
+        } else {
+            System.out.println("Element not found in the array");
+        }
+    }
+}
